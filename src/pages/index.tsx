316f3f2cfd322689e949fcf9ab5a6ac4be387eea
Robert Koch <robert@kochie.io>
2021-08-18 14:06:25 +1000

    added better cover photo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Index = () => {
           url: "https://candidate.kochie.io",
           title: "Candidate Statement for Robert Koch",
           description:
-            "I'm Robert and I'm running for a position on the Victorian division of Engineers Australia",
+            "I&apos;m Robert and I&apos;m running for a position on the Victorian division of Engineers Australia",
           images: [
             {
               url: "https://candidate.kochie.io/images/cover_photo.jpg",
