a2cfbcab1621558a35a66c46c86b2d95a233688d
Robert Koch <robert@kochie.io>
2021-08-18 17:05:47 +1000

    added fathom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,40 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 
 import "tailwindcss/tailwind.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 config.autoAddCss = false;
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load("ATVFHAFW", {
+      includedDomains: ["candidate.kochie.io"],
+      url: "kite.kochie.io",
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default App;
